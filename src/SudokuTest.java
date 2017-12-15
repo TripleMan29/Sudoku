2f64ff4489ebd70c3d31971c60903bc2a6566ece
TripleMan29 <deniska019283>
2017-12-15 11:06:04 +0300

    Просто добавлен один тест

--- src/SudokuTest.java
+++ src/SudokuTest.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class SudokuTest {
     @Test
     public void printSudokuSolution() throws Exception {
-        Sudoku.printSudokuSolution("Tests/test1.txt");
+        Sudoku.printSudokuSolution("Tests/test228.txt");
     }
 }
